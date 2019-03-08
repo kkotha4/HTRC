d8f83d5372dde5c4f6d03226b088c4197e49bb0f
Kashish <kkotha4@illinois.edu>
2019-03-08 10:31:00 -0600

    t

--- htrc_data_download.py
+++ htrc_data_download.py
@@ -13,21 +13,20 @@ else:
     # This code will execute when running `python -O script.py`
     # The -O argument turns on optimizations, setting __debug__ = False.
     import htrc.volumes as volumes
-    
+
 with open('/home/dcuser/kashish/HTRC/data.json') as f:
     data = json.load(f)
 #convert all the volume id's into list
 volume_ids=list(data)
 token=volumes.get_oauth2_token('kkothari442', 'K@shish99')
 print(token)
+vol=volumes.get_volumes(token, [volume_ids[0]], concat=True)
+print(vol)
 volume = [volume_ids[0]]
 output_directory="kk"
 
-volumes.download(volume) 
+volumes.download(volume,output_directory)
 
 
 print("completed")
 print(len(volume_ids))
- 
-
-    
