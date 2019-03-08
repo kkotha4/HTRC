0310eb437773af93bd5ec1357a7db408017e3623
Kashish <kkotha4@illinois.edu>
2019-03-07 23:50:19 -0600

    T

--- htrc_data_download.py
+++ htrc_data_download.py
@@ -6,12 +6,6 @@ Created on Thu Mar  7 21:19:56 2019
 @author: kashish
 """
 import json
-with open('/home/dcuser/kashish/HTRC/data.json') as f:
-    data = json.load(f)
-
-ls=list(data) 
-print(len(ls))
- 
 if __debug__:
     # This code will execute when running `python script.py`
     import htrc.mock.volumes as volumes
@@ -19,3 +13,20 @@ else:
     # This code will execute when running `python -O script.py`
     # The -O argument turns on optimizations, setting __debug__ = False.
     import htrc.volumes as volumes
+    
+with open('/home/dcuser/kashish/HTRC/data.json') as f:
+    data = json.load(f)
+#convert all the volume id's into list
+volume_ids=list(data)
+
+volume = [volume_ids[0]]
+output_directory="kk"
+
+volumes.download(volume, output_directory) 
+
+
+print("completed")
+print(len(volume_ids))
+ 
+
+    
